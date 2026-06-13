3daa0abf5637d44d0a47c71f180ebc8965c8a431
Timothy Legge <timlegge@gmail.com>
2026-06-13 17:46:46 +0200

    Support older versions of the bundle file

--- example/verify_sigstore.pl
+++ example/verify_sigstore.pl
@@ -44,10 +44,18 @@ sub _load_bundle {
 
 sub _get_der_from_bundle {
     my $bundle = shift;
-    my $raw_bytes = $bundle->{verificationMaterial}->{certificate}->{rawBytes};
-    return decode_base64($raw_bytes);
-}
 
+    my $cert;
+    if (defined $bundle->{mediaType} && $bundle->{mediaType} eq 'application/vnd.dev.sigstore.bundle.v0.3+json')
+    {
+        $cert = $bundle->{verificationMaterial}->{certificate}->{rawBytes};
+    } else {
+        $cert = decode_base64($bundle->{cert});
+        $cert =~ s/-----[^-]*-----//gm;
+    }
+
+    return decode_base64($cert);
+}
 sub _get_x509_from_der {
     my $der = shift;
     return Crypt::OpenSSL::X509->new_from_string(
